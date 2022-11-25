84f00ff1db94a0e336276c844b789f65684652a7
Fiodar Morau <chelovekdrakon@gmail.com>
2022-11-25 19:25:57 +0800

    fix: work with files

--- android/src/main/java/com/vinzscam/reactnativefileviewer/RNFileViewerModule.java
+++ android/src/main/java/com/vinzscam/reactnativefileviewer/RNFileViewerModule.java
@@ -73,7 +73,7 @@ public class RNFileViewerModule extends ReactContextBaseJavaModule {
       return;
     }
 
-    String extension = MimeTypeMap.getFileExtensionFromUrl(path).toLowerCase();
+    String extension = this.getFileExtensionFromUrl(path).toLowerCase();
     String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
 
     Intent shareIntent = new Intent();
@@ -137,4 +137,33 @@ public class RNFileViewerModule extends ReactContextBaseJavaModule {
 
   @ReactMethod
   public void removeListeners(Integer count) { }
+
+  public static String getFileExtensionFromUrl(String url) {
+    if (!TextUtils.isEmpty(url)) {
+      int fragment = url.lastIndexOf('#');
+      if (fragment > 0) {
+        url = url.substring(0, fragment);
+      }
+
+      int query = url.lastIndexOf('?');
+      if (query > 0) {
+        url = url.substring(0, query);
+      }
+
+      int filenamePos = url.lastIndexOf('/');
+      String filename =
+              0 <= filenamePos ? url.substring(filenamePos + 1) : url;
+
+      // if the filename contains special characters, we don't
+      // consider it valid for our matching purposes:
+      if (!filename.isEmpty()) {
+        int dotPos = filename.lastIndexOf('.');
+        if (0 <= dotPos) {
+          return filename.substring(dotPos + 1);
+        }
+      }
+    }
+
+    return "";
+  }
 }
